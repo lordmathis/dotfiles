c36f047564ed18d8d098b1d6e802120f07d3c701
LordMathis <namesny.matus@gmail.com>
2018-05-25 23:47:28 +0200

    Add scripts

---
+++ bin/lock.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+letterEnteredColor=00af46ff
+letterRemovedColor=d23c3dff
+passwordCorrect=00000000
+passwordIncorrect=d23c3dff
+background=00000000
+foreground=ffffffff
+i3lock \
+	-t -i "$1" \
+	--timepos="110:h-70" \
+	--datepos="135:h-45" \
+	--clock --datestr "Type password to unlock..." \
+	--insidecolor=$background --ringcolor=$foreground --line-uses-inside \
+	--keyhlcolor=$letterEnteredColor --bshlcolor=$letterRemovedColor --separatorcolor=$background \
+	--insidevercolor=$passwordCorrect --insidewrongcolor=$passwordIncorrect \
+	--ringvercolor=$foreground --ringwrongcolor=$foreground --indpos="x+280:h-70" \
+	--radius=20 --ring-width=4 --veriftext="" --wrongtext="" \
+	--verifcolor="$foreground" --timecolor="$foreground" --datecolor="$foreground" \
+    --noinputtext="" --force-clock $lockargs
