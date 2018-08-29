ed1d1cf189a56cb6b8bc6bd933d42164fb6274dd
LordMathis <namesny.matus@gmail.com>
2018-08-29 21:01:04 +0200

    Update giconfig

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,3 +3,11 @@
 echo "Enter OpenWeatherMap API key"
 read line
 echo $line | secret-tool store --label='OpenWeatherMap API Key' openweathermap api_key
+
+echo "Enter git name"
+read name
+git config --global user.name $name
+
+echo "Enter git email"
+read email
+git config --global user.email $email
