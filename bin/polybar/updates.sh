6728f787ccc913e853b17720c5863354bd57c273
LordMathis <namesny.matus@gmail.com>
2020-01-07 14:16:14 +0100

    Remove unnecessary files

--- bin/polybar/updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-updates_arch=$(checkupdates | wc -l)
-
-if [ "$updates_arch" -gt 0 ]; then
-    echo " $updates_arch"
-else
-    echo ""
-fi
--- bin/startup/start_keepassxc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-secret-tool lookup keepassxc master | keepassxc --pw-stdin ~/Documents/Important/passwords.kdbx
