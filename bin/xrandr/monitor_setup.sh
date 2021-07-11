7cf8537c07595cba635040b5dcba7a4a019fec78
LordMathis <matus@namesny.com>
2021-07-11 16:56:37 +0200

    Cleanup

--- bin/xrandr/monitor_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-left=DP-1-2-2
-middle=DP-1-2-3
-right=eDP-1-1
-
-xrandr --output "$middle" --auto --primary && xrandr --output "$left" --auto --left-of "$middle" && xrandr --output "$right" --auto --right-of "$middle"
