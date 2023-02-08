0d4d2de665d72057bd8ed8c5e865cbca8b3f3fe3
Mathis <matus@namesny.com>
2023-02-08 20:40:05 +0000

    Update 'install.sh'

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ if [ $# -eq 0 ]
 fi
 
 echo ".dotfiles" >> $HOME/.gitignore
-git clone --bare git@git.namesny.com:Mathis/dotfiles.git $HOME/.dotfiles
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-config checkout $1
-config submodule init
-config submodule update
+git clone --bare https://git.namesny.com/Mathis/dotfiles.git $HOME/.dotfiles &&
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' &&
+config checkout $1 &&
+config submodule init &&
+config submodule update &&
 source .zshrc
